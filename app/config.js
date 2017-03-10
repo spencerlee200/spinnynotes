6dfe7a29ece52b89b85406bd311ff91630256515
Spencer <SpencersMac@Spencers-MacBook-Pro.local>
2017-03-09 23:15:40 -0500

    Fixing oauth for the 100th time

--- app/config.js
+++ app/config.js
@@ -1,13 +1,13 @@
 var github = {
   clientID: '85c5da0fd2f70f73affc',
   clientSecret: 'b263b07ef930222140bce4631002d39843456ab1',
-  callbackURL: "http://174.138.69.73/auth/github/callback"
+  callbackURL: "http://spinnynotes.com/auth/github/callback"
 }
 
 var google = {
   clientID: '708247688332-d0pj8ho0q4t4e8qcp59k0gognv1dl5du.apps.googleusercontent.com',
   clientSecret: 'bpMYJojvU-oVypOmtp4g9isU',
-  callbackURL: 'http://174.138.69.73/google/callback/'
+  callbackURL: 'http://spinnynotes.com/google/callback/'
 }
 
 module.exports = {github: github, google: google};
